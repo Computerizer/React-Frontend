2d97ad57e06d0d67f2037ee062b11c787027c86d
Moath Abdelaziz <93261842+Moath-COE@users.noreply.github.com>
2023-06-19 21:22:39 +0300

    installing next js and config

---
+++ app/blog/page.js
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <p className="bg-gray-700">this is the blog routing page</p>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/page.js
@@ -0,0 +1,16 @@
+const page = () => {
+  return (
+    <div>
+      <p className="flex">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque soluta
+        odio molestias ipsum dicta voluptate illum. Dolore inventore iure cum
+        sapiente quia nam possimus? Ea quo consequuntur iure, illum soluta quos,
+        doloremque, maiores illo aperiam molestias quaerat veritatis. Molestias
+        pariatur eum modi possimus minima quod magnam praesentium nobis ullam
+        dignissimos?
+      </p>
+    </div>
+  );
+};
+
+export default page;
